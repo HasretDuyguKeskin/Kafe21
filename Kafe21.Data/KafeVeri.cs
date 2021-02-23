8124afae2c15271bd7ed8d33035fc5145655dce3
Hasret Keskin <hasret_1989_@hotmail.com>
2021-02-23 18:15:11 +0300

    Ef6 Entegrasyon

--- Kafe21.Data/KafeVeri.cs
+++ Kafe21.Data/KafeVeri.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Kafe21.Data
 {
-    public class KafeVeri
+    public class KafeVeri : DbContext
     {
+        public KafeVeri() : base("name=KafeVeri")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SiparisDetay>().HasRequired(x => x.Urun).WithMany(x => x.SiparisDetaylar).HasForeignKey(x => x.UrunId).WillCascadeOnDelete(false);
+        }
         public int MasaAdet { get; set; } = 24;
-        public List<Siparis> AktifSiparisler { get; set; } = new List<Siparis>();
-        public List<Urun> Urunler { get; set; } = new List<Urun>();
-        public List<Siparis> GecmisSiparisler { get; set; } = new List<Siparis>();
+        public DbSet<Urun> Urunler { get; set; }
+        public DbSet<Siparis> Siparisler { get; set; }
+        public DbSet<SiparisDetay> SiparisDetaylar { get; set; }
     }
 }
--- Kafe21.Data/Siparis.cs
+++ Kafe21.Data/Siparis.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Kafe21.Data
 {
+    [Table("Siparisler")]
     public class Siparis
     {
-
+        public int Id { get; set; }
         public int MasaNo { get; set; }
         public decimal OdenenTutar { get; set; }
         public DateTime? AcilisZamani { get; set; } = DateTime.Now;
         public DateTime? KapanisZamani { get; set; }
-        public List<SiparisDetay> SiparisDetaylar { get; set; } = new List<SiparisDetay>();
+        public virtual ICollection<SiparisDetay> SiparisDetaylar { get; set; } = new HashSet<SiparisDetay>();
         public SiparisDurum Durum { get; set; }
         public string ToplamTutarTL => ToplamTutar().ToString("c2");
         public decimal ToplamTutar()
